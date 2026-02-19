d686e502f5dcad0b42406e505e118962f85d576e
Eyal Amir <eyalamirmusic@gmail.com>
2026-02-18 20:44:11 -0800

    Fixed basicuse test to read from the right runtime category

--- Tests/BasicUse/main.cpp
+++ Tests/BasicUse/main.cpp
@@ -1,18 +1,18 @@
 #include <BinaryData.h>
+#include "Resources.h"
 #include <iostream>
 
-void printResource(const std::string& name, const std::string& category = "BinaryData")
+void printResource(const std::string& name, const std::string& category)
 {
-    auto d = ResEmbed::get(name, "Custom");
+    auto d = ResEmbed::get(name, category);
     std::cout << d.toString() << std::endl;
 }
 
 int main()
 {
-    printResource("data.bin");
-    printResource("data2.txt");
-    printResource("SRC-ND_INST-Clap_NAME-Shlap!.flac");
-    printResource("data2.txt", "ResEmbed");
+    printResource("data.bin", "Custom");
+    printResource("data2.txt", "Custom");
+    printResource("data2.txt", "Resources");
 
     return 0;
 }
